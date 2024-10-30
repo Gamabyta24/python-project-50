32026a152efec1d3e16ef638731e31c5d1b4c758
Gamabyta24 <Gamabyta@gmail.com>
2024-10-30 19:10:13 +0300

    relize pars two json files

--- script/gendiff.py
+++ script/gendiff.py
@@ -1,16 +1,28 @@
 import argparse
+import json
+
+def read_json_file(file_path):
+    """Читает и парсит JSON-файл."""
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
 def main():
     parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
     
-    # добавляем любые другие опции
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f','--format',help='set format of output')
-    
+    # Определяем аргументы
+    parser.add_argument('first_file', help='первый файл для сравнения')
+    parser.add_argument('second_file', help='второй файл для сравнения')
+    parser.add_argument('-f', '--format', help='установить формат вывода')
+    parser.add_argument('-r', '--read', help='прочитать файл')
+
     args = parser.parse_args()
 
-    # Ваш основной код
-    print(f'Аргумент echo: {args.echo}')
+    # Чтение и парсинг файлов
+    data1 = read_json_file(args.first_file)
+    data2 = read_json_file(args.second_file)
+
+    print("Содержимое", args.first_file, ":", data1)
+    print("Содержимое", args.second_file, ":", data2)
 
 if __name__ == '__main__':
     main()
