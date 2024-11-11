30166ff629857212ef36020282af20ec27881ab4
Gamabyta24 <Gamabyta@gmail.com>
2024-11-11 17:42:20 +0300

    fix build_diff

--- gendiff/build_diff.py
+++ gendiff/build_diff.py
@@ -1,3 +1,42 @@
+# def build_diff(data1, data2):
+#     diff = []
+
+#     all_keys = sorted(set(data1.keys()).union(data2.keys()))
+
+#     for key in all_keys:
+#         if key not in data2:
+#             diff.append({"key": key, "status": "removed", "value": data1[key]})
+#         elif key not in data1:
+#             diff.append({"key": key, "status": "added", "value": data2[key]})
+#         elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
+#             diff.append(
+#                 {
+#                     "key": key,
+#                     "status": "nested",
+#                     "children": build_diff(data1[key], data2[key]),
+#                 }
+#             )
+#         elif data1[key] == data2[key]:
+#             diff.append(
+#                 {"key": key, "status": "unchanged", "value": data1[key]})
+#         else:
+#             diff.append(
+#                 {
+#                     "key": key,
+#                     "status": "updated",
+#                     "old_value": data1[key],
+#                     "new_value": data2[key],
+#                 }
+#             )
+#     print(diff)
+#     return diff
+def format_value(value):
+    if value is True:
+        return "true"
+    elif value is False:
+        return "false"
+    return value
+
 def build_diff(data1, data2):
     diff = []
 
@@ -5,9 +44,9 @@ def build_diff(data1, data2):
 
     for key in all_keys:
         if key not in data2:
-            diff.append({"key": key, "status": "removed", "value": data1[key]})
+            diff.append({"key": key, "status": "removed", "value": format_value(data1[key])})
         elif key not in data1:
-            diff.append({"key": key, "status": "added", "value": data2[key]})
+            diff.append({"key": key, "status": "added", "value": format_value(data2[key])})
         elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
             diff.append(
                 {
@@ -18,15 +57,14 @@ def build_diff(data1, data2):
             )
         elif data1[key] == data2[key]:
             diff.append(
-                {"key": key, "status": "unchanged", "value": data1[key]})
+                {"key": key, "status": "unchanged", "value": format_value(data1[key])})
         else:
             diff.append(
                 {
                     "key": key,
                     "status": "updated",
-                    "old_value": data1[key],
-                    "new_value": data2[key],
+                    "old_value": format_value(data1[key]),
+                    "new_value": format_value(data2[key]),
                 }
             )
-
     return diff
